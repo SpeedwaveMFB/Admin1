5eb0a5b2563e3c0897131c19dce05d82075b37e6
Youngestage <youngleslie.ik@gmail.com>
2026-05-18 15:08:07 +0100

    hh

--- src/types/react-table.d.ts
+++
@@ -1,9 +0,0 @@
-import '@tanstack/react-table';
-
-declare module '@tanstack/react-table' {
-  interface ColumnMeta<TData, TValue> {
-    headerClassName?: string;
-    cellClassName?: string;
-    width?: string;
-  }
-}
